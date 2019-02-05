998a76dcf9948a9aad6c2306125845e5b96baec2
arthurarty <arthur.nangai@gmail.com>
2019-02-05 21:03:40 +0300

    ft(signup) - Add signup.
    - Add redux store.
    - Add signu form and view.
    - Add tests.
    
    [Delivers #163695469]

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Routes from './routes/index';
 
 class Index extends React.Component {
---
+++ src/components/auth/SignUpForm.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextInput from '../common/TextInput';
+import PasswordInput from '../common/PasswordInput';
+import NumberInput from '../common/NumberInput';
+
+const SignUpForm = ({
+  user, onSave, onChange,
+}) => {
+  return (
+    <div id="signUpForm" className="col-lg-12 col-md-12 col-sm-12">
+      <form>
+        <h1 className="text-center">Sign Up</h1>
+        <TextInput
+          name="email"
+          label="Email"
+          value={user.email}
+          onChange={onChange}
+        />
+        <TextInput
+          name="name"
+          label="Name"
+          value={user.name}
+          onChange={onChange}
+        />
+        <PasswordInput
+          name="password"
+          label="Password"
+          value={user.password}
+          onChange={onChange}
+        />
+        <NumberInput
+          name="role"
+          label="Role"
+          value={user.role}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Sign UP"
+          className="btn btn-primary btn-block"
+          onClick={onSave}
+        />
+      </form>
+    </div>
+  );
+};
+
+SignUpForm.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string,
+  }).isRequired,
+  onSave: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+
+export default SignUpForm;
